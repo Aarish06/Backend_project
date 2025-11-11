ccc58ad3dd9e8b19de3578a9c42f2068506a4f8f
rrc_f2024_abansal2 <abansal2@rrc.ca>
2025-11-10 19:01:02 -0600

    updated app.ts to test the server

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Express } from "express";
+import bookRoutes from "../src/api/v1/routes/bookRoutes";
+import userRoutes from "../src/api/v1/routes/userRoutes";
+import borrowRoutes from "../src/api/v1/routes/borrowerRoutes";
+
+// Initialize Express application
+const app: Express = express();
+
+// Define a route
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.use("/books", bookRoutes);
+app.use("/users", userRoutes);
+app.use("/borrows", borrowRoutes);
+
+
+
+export default app;
